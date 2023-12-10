35a6dea81c80d4690066d93c12ba65700dfc61c9
Yagiz Avsar <63153577+AvsarYagiz@users.noreply.github.com>
2023-12-10 22:19:01 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,36 +1,66 @@
 // Create a web server
-// Create a route to handle GET requests
-// Create a route to handle POST requests
-// Create a route to handle DELETE requests
+// 1. add a comment
+// 2. delete a comment
+// 3. modify a comment
+// 4. list comments
 
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-// Import the Comment model
-const Comment = require('../models/comment');
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
 
-// Handle GET requests
-router.get('/', (req, res, next) => {
-    // res.send('GET request to /comment endpoint');
-    Comment.find()
-        .exec()
-        .then(docs => {
-            // console.log(docs);
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            // console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+app.locals.pretty = true;
+
+app.set('views', './views_file');
+app.set('view engine', 'jade');
+
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
 });
 
-// Handle POST requests
-router.post('/', (req, res, next) => {
-    // res.send('POST request to /comment endpoint');
-    // Create a new Comment model
-    const comment = new Comment({
-        _id: new mongoose.Types.ObjectId()
-    });
+app.get('/topic/new', function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
+});
+
+app.get(['/topic', '/topic/:id'], function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.id;
+    if(id){
+      // id 값이 있을 때
+      fs.readFile('data/'+id, 'utf8', function(err, data){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics:files, title:id, description:data});
+      });
+    } else {
+      // id 값이 없을 때
+      res.render('view', {topics:files, title:'Welcome', description:'Hello, JavaScript for server.'});
+    }
+  });
+});
+
+app.post('/topic', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/'+title);
+  });
 });
